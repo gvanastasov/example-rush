b3eb436fc5f86a8f3d0dbbccd83531cb03beecf6
Georgi Anastasov <14071890+gvanastasov@users.noreply.github.com>
2023-01-31 22:22:35 +0100

    add transpilar for components

--- apps/example-calculator/src/App.js
+++ apps/example-calculator/src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { Button } from 'example-components'
+
 function App() {
   return (
     <div className="App">
+      <Button>Example Button</Button>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
