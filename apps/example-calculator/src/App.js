7bda2309ad5a49da12d227549e76bc35ad9037b1
Georgi Anastasov <14071890+gvanastasov@users.noreply.github.com>
2023-02-01 23:01:02 +0100

    simple calculator app

--- apps/example-calculator/src/App.js
+++ apps/example-calculator/src/App.js
@@ -2,27 +2,126 @@ import logo from './logo.svg';
 import './App.css';
 
 import { Button } from 'example-components'
+import * as operations from 'example-math'
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <Button>Example Button</Button>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          CRUD <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const DEFAULT_STATE = {
+  display: '',
+  op: undefined,
+  eq: '',
+  operand1: 0,
+  renew: false
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.init()
+  }
+
+  init() {
+    this.state = { ...DEFAULT_STATE }
+  }
+
+  reset() {
+    this.setState({ ...DEFAULT_STATE })
+  }
+  
+  clearDisplay() {
+    this.state.display = ''
+  }
+
+  append(val) {
+    if (this.state.renew) {
+      this.reset()
+    }
+
+    this.setState({ 
+      display: this.state.display + val
+    })
+  }
+
+  revert(num) {
+    const outsideRange = this.state.display.length - num < 0    
+    if (outsideRange) {
+      return
+    }
+
+    this.setState({
+      display: this.state.display.slice(0, -num)
+    })
+  }
+
+  setOperation(op) {
+    this.setState({ 
+      op, 
+      operand1: Number(this.state.display), 
+      eq: `${this.state.display} ${this.getOperationSign(op)}` 
+    })
+    this.clearDisplay()
+  }
+
+  getOperationSign(op) {
+    return {
+      'add': '+',
+    }[op]
+  }
+  
+  eval() {
+    const noOperation = !this.state.op
+    const alreadyEvaluated = this.state.renew
+
+    if (noOperation || alreadyEvaluated) {
+      return;
+    }
+
+    const operand2 = Number(this.state.display)
+    const op = operations[this.state.op]
+    const result = op(this.state.operand1, operand2)
+
+    this.setState({ 
+      display: result,
+      eq: `${this.state.eq} ${operand2} =`,
+      renew: true
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="calculator">
+          <div className="calculator-equation">{this.state.eq == '' ? '\u00A0' : this.state.eq}</div>
+          <div className="calculator-display">{this.state.display == '' ? '0' : this.state.display}</div>
+          <div className="calculator-keyboard">
+            <button disabled></button>
+            <button onClick={() => this.clearDisplay()}>CE</button>
+            <button onClick={() => this.reset()}>C</button>
+            <button onClick={() => this.revert(1)}>&#60;</button>
+
+            <button onClick={() => this.append('7')}>7</button>
+            <button onClick={() => this.append('8')}>8</button>
+            <button onClick={() => this.append('9')}>9</button>
+            <button>x</button>
+            
+            <button onClick={() => this.append('4')}>4</button>
+            <button onClick={() => this.append('5')}>5</button>
+            <button onClick={() => this.append('6')}>6</button>
+            <button>-</button>
+
+            <button onClick={() => this.append('1')}>1</button>
+            <button onClick={() => this.append('2')}>2</button>
+            <button onClick={() => this.append('3')}>3</button>
+            <button onClick={() => this.setOperation('add')}>+</button>
+            
+            <button>+/-</button>
+            <button onClick={() => this.state.display ? this.append('0') : null}>0</button>
+            <button>,</button>
+            <button onClick={() => this.eval()} className="secondary">=</button>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
